523d1d4274f661732f7cbd24e9dd0b0eb74805cc
Víctor Guzmán Contreras <thevictorxx@gmail.com>
2023-11-28 21:50:33 -0300

    add: Formulario para buscar paciente por Run

--- src/views/BuscarPacienteVista.vue
+++ src/views/BuscarPacienteVista.vue
@@ -1,5 +1,40 @@
 <template>
   <div>
-    <h1>Buscar Paciente</h1>
+    <h1 class="text-center mb-3">Buscar Paciente</h1>
+
+    <v-row class="fill-height">
+      <v-col class="d-flex justify-center">
+        <v-card max-width="500" width="500" elevation="3">
+          <v-card-text>
+            <v-form ref="form" v-model="valid" lazy-validation>
+              <v-row>
+                <v-col cols="10">
+                  <v-text-field label="RUN" v-model="buscarRun" prepend-icon="mdi-account"></v-text-field>
+                </v-col>
+                <v-col cols="2">
+                  <v-btn type="submit" class="mt-3" block color="success">
+                    <v-icon>mdi-magnify</v-icon>
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'BuscarPacienteVista',
+  data () {
+    return {
+      buscarRun: ''
+    }
+  },
+  components: {
+    //
+  }
+}
+</script>
