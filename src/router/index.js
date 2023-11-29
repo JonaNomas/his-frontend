5849e64ffc8b86f00f00fbd88d6b4086e116c1c5
Víctor Guzmán Contreras <thevictorxx@gmail.com>
2023-11-28 21:18:11 -0300

    add: Autentificacion para rutas

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import store from '../store/index.js'
 
 Vue.use(VueRouter)
 
@@ -8,67 +9,119 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    meta: {
+      requiereAutentificacion: true,
+      permisosRequeridos: []
+    }
   },
   {
     path: '/agendamientoHoras',
     name: 'agendamientoHoras',
-    component: () => import(/* webpackChunkName: "about" */ '../views/AgendamientoHorasVista.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/AgendamientoHorasVista.vue'),
+    meta: {
+      requiereAutentificacion: true,
+      permisosRequeridos: []
+    }
   },
   {
     path: '/ingresar',
     name: 'ingresar',
-    component: () => import(/* webpackChunkName: "about" */ '../views/IngresarVista.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/IngresarVista.vue'),
+    meta: {
+      requiereAutentificacion: false,
+      permisosRequeridos: []
+    }
   },
   {
     path: '/usuario',
     name: 'usuario',
-    component: () => import(/* webpackChunkName: "about" */ '../views/UsuarioVista.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/UsuarioVista.vue'),
+    meta: {
+      requiereAutentificacion: true,
+      permisosRequeridos: []
+    }
   },
   {
     path: '/buscarPaciente',
     name: 'buscarPaciente',
-    component: () => import(/* webpackChunkName: "about" */ '../views/BuscarPacienteVista.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/BuscarPacienteVista.vue'),
+    meta: {
+      requiereAutentificacion: true,
+      permisosRequeridos: []
+    }
   },
   {
     path: '/nuevoPaciente',
     name: 'nuevoPaciente',
-    component: () => import(/* webpackChunkName: "about" */ '../views/IngresarNuevoPaciente.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/IngresarNuevoPaciente.vue'),
+    meta: {
+      requiereAutentificacion: true,
+      permisosRequeridos: []
+    }
   },
   {
     path: '/atencionUrgencias',
     name: 'atencionUrgencias',
-    component: () => import(/* webpackChunkName: "about" */ '../views/AtencionUrgenciaVista.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/AtencionUrgenciaVista.vue'),
+    meta: {
+      requiereAutentificacion: true,
+      permisosRequeridos: []
+    }
   },
   {
     path: '/hospitalizacion',
     name: 'hospitalizacion',
-    component: () => import(/* webpackChunkName: "about" */ '../views/HospitalizacionVista.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/HospitalizacionVista.vue'),
+    meta: {
+      requiereAutentificacion: true,
+      permisosRequeridos: []
+    }
   },
   {
     path: '/cirugia',
     name: 'cirugia',
-    component: () => import(/* webpackChunkName: "about" */ '../views/CirugiasVista.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/CirugiasVista.vue'),
+    meta: {
+      requiereAutentificacion: true,
+      permisosRequeridos: []
+    }
   },
   {
     path: '/atencionesMedicas',
     name: 'atencionesMedicas',
-    component: () => import(/* webpackChunkName: "about" */ '../views/AtencionesMedicas.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/AtencionesMedicas.vue'),
+    meta: {
+      requiereAutentificacion: true,
+      permisosRequeridos: []
+    }
   },
   {
     path: '/imagenologia',
     name: 'imagenologia',
-    component: () => import(/* webpackChunkName: "about" */ '../views/ImagenologiaVista.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/ImagenologiaVista.vue'),
+    meta: {
+      requiereAutentificacion: true,
+      permisosRequeridos: []
+    }
   },
   {
     path: '/laboratorio',
     name: 'laboratorio',
-    component: () => import(/* webpackChunkName: "about" */ '../views/LaboratorioVista.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/LaboratorioVista.vue'),
+    meta: {
+      requiereAutentificacion: true,
+      permisosRequeridos: []
+    }
   },
   {
     path: '/bancoSangre',
     name: 'bancoSangre',
-    component: () => import(/* webpackChunkName: "about" */ '../views/BancoSangreVista.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/BancoSangreVista.vue'),
+    meta: {
+      requiereAutentificacion: true,
+      permisosRequeridos: []
+    }
   }
 ]
 
@@ -76,6 +129,18 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(route => route.meta.requiereAutentificacion)) {
+    if (!store.state.estaLoggeado) {
+      next({ path: '/ingresar' })
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,32 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    estaLoggeado: true,
+    usuario: {
+      nombre: '',
+      apellidoPaterno: '',
+      apellidoMaterno: '',
+      run: '',
+      email: '',
+      foto: '',
+      uid: ''
+    },
+    layout: {
+      drawer: {
+        visible: true,
+        mini: false
+      }
+    }
   },
   getters: {
   },
   mutations: {
+    invertirLayoutDrawerMini (state) {
+      state.layout.drawer.mini = !state.layout.drawer.mini
+    }
   },
   actions: {
+
   },
   modules: {
   }
