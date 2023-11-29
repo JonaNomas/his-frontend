e16d5e502238efb34e9d388d187aa6baccd9cead
Víctor Guzmán Contreras <thevictorxx@gmail.com>
2023-11-28 21:17:40 -0300

    add: Nueva paleta de colores

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -11,9 +11,9 @@ export default new Vuetify({
     },
     themes: {
       light: {
-        primary: '#007BFF',
-        secondary: '#424242',
-        accent: '#82B1FF',
+        primary: '#00334e',
+        secondary: '#004d73',
+        accent: '#007fbd',
         error: '#FF5252',
         info: '#2196F3',
         success: '#4CAF50',
@@ -26,3 +26,18 @@ export default new Vuetify({
     current: 'es'
   }
 })
+/*
+
+themes: {
+    light: {
+    primary: '#007BFF',
+    secondary: '#424242',
+    accent: '#82B1FF',
+    error: '#FF5252',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FFC107'
+  }
+}
+
+*/
