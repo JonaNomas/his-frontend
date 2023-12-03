98479216be75879bb8a39f46760b909442689720
Víctor Guzmán Contreras <thevictorxx@gmail.com>
2023-12-03 15:42:57 -0300

    add: Configuracion de Axios

---
+++ src/services/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const API_URL = 'https://api.medsoft.cl/'
+
+export default () => {
+  return axios.create({
+    baseURL: API_URL,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + localStorage.getItem('token')
+    }
+  })
+}
