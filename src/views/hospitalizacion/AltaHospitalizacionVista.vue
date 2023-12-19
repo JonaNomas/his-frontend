f6db6fe74334f61f52ab683b080e1d07556d9358
Víctor Guzmán Contreras <thevictorxx@gmail.com>
2023-12-19 02:09:19 -0300

    add: Alta vista hospitalizacion

--- src/views/hospitalizacion/AltaHospitalizacionVista.vue
+++ src/views/hospitalizacion/AltaHospitalizacionVista.vue
@@ -11,7 +11,7 @@
         <v-card>
           <v-card-title>
             <v-row>
-              <v-col>Listado pacientes en atención</v-col>
+              <v-col>Listado pacientes hospitalizados</v-col>
               <v-col><v-text-field dense v-model="filtroPacientes" append-icon="mdi-magnify" outlined
                   label="Filtrar Pacientes" hide-details></v-text-field></v-col>
             </v-row>
@@ -38,13 +38,13 @@
     <v-dialog v-model="dialogAlta" max-width="700">
       <v-card>
         <v-toolbar color="primary" dark>
-          <v-toolbar-title><v-icon left>mdi-plus</v-icon> Antecedentes Morbidos</v-toolbar-title>
+          <v-toolbar-title><v-icon left>mdi-close-box</v-icon> Alta Hospitalización</v-toolbar-title>
         </v-toolbar>
 
         <v-card-text>
           <v-row class="mt-5">
             <v-col>
-              Alta
+              <p class="text-h3 text-center">Se cerrara de forma permanente esta Hospitalización, ¿Desea continuar?</p>
             </v-col>
           </v-row>
         </v-card-text>
@@ -52,8 +52,11 @@
         <v-card-actions>
           <v-spacer></v-spacer>
 
+          <v-btn color="secondary" text @click="dialogAlta = false">
+            Cancelar
+          </v-btn>
           <v-btn color="error" text @click="dialogAlta = false">
-            Cerrar
+            Dar de Alta
           </v-btn>
         </v-card-actions>
       </v-card>
@@ -76,19 +79,20 @@ export default {
         { text: 'ID', value: 'idAtencion', align: 'center' },
         { text: 'Rut', value: 'rut', align: 'center' },
         { text: 'Nombre', value: 'nombre' },
-        { text: 'Motivo', value: 'motivo' },
+        { text: 'Diagnostico', value: 'diagnostico' },
+        { text: 'Cama', value: 'cama' },
         { text: 'Acciones', value: 'actions', sortable: false, align: 'center' }
       ],
       listadoPacientesEnAtencion: [
-        { idAtencion: 241, rut: '12345678-5', nombre: 'Victor Alexis Guzman Contreras', motivo: 'Corte - Cabeza' },
-        { idAtencion: 250, rut: '19876543-2', nombre: 'Valentina Isabella González Ramírez', motivo: 'Dolor - Cabeza' },
-        { idAtencion: 251, rut: '17654321-0', nombre: 'Diego Andrés Martínez Castro', motivo: 'Quemadura - Torax' },
-        { idAtencion: 252, rut: '12345678-9', nombre: 'Camila Sofía Fernández Díaz', motivo: 'Corte - Cuello' },
-        { idAtencion: 254, rut: '11098765-4', nombre: 'Mateo Nicolás Pérez Herrera', motivo: 'Fractura-Extremidades   Superiores' },
-        { idAtencion: 256, rut: '13210987-6', nombre: 'Isabella Gabriela López Ramírez', motivo: 'Perdida de Conocimiento - Otro' },
-        { idAtencion: 270, rut: '18901234-5', nombre: 'Andrés Alejandro Díaz Castro', motivo: 'Hemorragia - Espalda' },
-        { idAtencion: 271, rut: '14567890-1', nombre: 'Sofía Valentina Herrera González', motivo: 'Molestia - Abdomen' },
-        { idAtencion: 273, rut: '17654321-9', nombre: 'Nicolás Diego Martínez López', motivo: 'Perdida Extremidad - Extremidades nferiores' }
+        { idAtencion: 12, rut: '17654321-0', nombre: 'Diego Andrés Martínez Castro', diagnostico: 'Quemadura - Torax', cama: '301' },
+        { idAtencion: 15, rut: '12345678-5', nombre: 'Victor Alexis Guzman Contreras', diagnostico: 'Corte - Cabeza', cama: '302' },
+        { idAtencion: 20, rut: '19876543-2', nombre: 'Valentina Isabella González Ramírez', diagnostico: 'Dolor - Cabeza', cama: '303' },
+        { idAtencion: 22, rut: '11098765-4', nombre: 'Mateo Nicolás Pérez Herrera', diagnostico: 'Fractura-Extremidades   Superiores', cama: '401' },
+        { idAtencion: 23, rut: '12345678-9', nombre: 'Camila Sofía Fernández Díaz', diagnostico: 'Corte - Cuello', cama: '402' },
+        { idAtencion: 32, rut: '13210987-6', nombre: 'Isabella Gabriela López Ramírez', diagnostico: 'Perdida de Conocimiento - Otro', cama: '403' },
+        { idAtencion: 33, rut: '14567890-1', nombre: 'Sofía Valentina Herrera González', diagnostico: 'Molestia - Abdomen', cama: '404' },
+        { idAtencion: 34, rut: '17654321-9', nombre: 'Nicolás Diego Martínez López', diagnostico: 'Perdida Extremidad - Extremidades nferiores', cama: '201' },
+        { idAtencion: 35, rut: '18901234-5', nombre: 'Andrés Alejandro Díaz Castro', diagnostico: 'Hemorragia - Espalda', cama: '203' }
       ]
     }
   },
